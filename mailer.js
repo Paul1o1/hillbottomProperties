003fa8261d0475c27c5bff3c43c290200f76b46f
pablo <pablonigussie1@gmail.com>
2024-06-17 20:42:29 +0300

    preloaded some scripts

---
+++ mailer.js
@@ -0,0 +1,8 @@
+
+function ml_webform_success_15283467() {
+var $ = ml_jQuery || jQuery;
+$('.ml-subscribe-form-15283467 .row-success').show();
+$('.ml-subscribe-form-15283467 .row-form').hide();
+                } 
+
+    fetch("https://assets.mailerlite.com/jsonp/969860/forms/122929447754008139/takel")
